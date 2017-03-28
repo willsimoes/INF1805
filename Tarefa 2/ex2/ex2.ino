1a269a0e056c36f268e2186351b60bda516dd3ca
Willian Simões <willian.smos@gmail.com>
2017-03-28 07:23:09 -0300

    (Tarefa 2) Melhorando código

--- Tarefa 2/ex2/ex2.ino
+++ Tarefa 2/ex2/ex2.ino
@@ -1,6 +1,7 @@
 #define LED_PIN 13
 #define BUT1_PIN A1
 #define BUT2_PIN A2
+#include <stdlib.h>
 
 int ledState = HIGH;
 int but1State = HIGH;
@@ -11,6 +12,7 @@ long interval = 1000;
 unsigned long current = 0;
 // the setup function runs once when you press reset or power the brd
 void setup() {
+  Serial.begin(9600);
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_PIN, OUTPUT);
 }
@@ -19,6 +21,9 @@ void setup() {
 void loop() {
   
   unsigned long time = millis();
+  unsigned long time_but1 = 0;
+  unsigned long time_but2 = 0;
+  
   digitalWrite(LED_PIN, ledState);
   if(time - current >= interval) {
      current = time;
@@ -34,11 +39,26 @@ void loop() {
 
   if (currentBut1State==LOW && but1State==HIGH) {
       interval += 50;
+      time_but1 = millis();
   } 
   
   if (currentBut2State==LOW && but2State==HIGH) {
       interval -= 50;
+      time_but2 = millis();
   } 
+
+  unsigned long diff_time;
+  if(currentBut1State==LOW && currentBut2State==LOW) {
+    diff_time = time_but1 - time_but2;
+    if(diff_time < -1) {
+      diff_time = time_but2 - time_but1;
+    } 
+    
+    if(diff_time<= 500) {
+      digitalWrite(LED_PIN, HIGH);
+      exit(1);
+    }
+  }
 
   if(currentBut1State != but1State) {
     but1State = currentBut1State;
