19932d999be76ad8f76c6aba5d41f60614654787
Willian Simões <willian.smos@gmail.com>
2017-04-04 01:11:17 -0300

    (Mini Projeto 1)  Acrescentando interrupção para botões

--- mini-arduino/mini_projeto/mini_projeto.ino
+++ mini-arduino/mini_projeto/mini_projeto.ino
@@ -2,18 +2,29 @@
 #define ECHO_PIN 2 // Pino ECHO do sensor que recebe o pulso ultrassônico de volta
 #define BUZZ_PIN 9 // Pino do Buzzer
 
+// Pinos do led RGB
 #define LED_RED     12
 #define LED_GREEN   11
 #define LED_BLUE    10
 
-#define KEY_LEFT    34
-#define KEY_RIGHT   35
+// Pinos dos botoes
+#define BUT_DOWN    36
+#define BUT_UP      37
+
 
 float tempo;
 float distancia;
 
+int modeBeep = 0;
+
 int atraso = 1000;
-int duration = 200;
+
+//declaracao das funcoes
+void desligarLeds();
+void corLed(int red, int green, int blue);
+void acendeLed(float distancia);
+void tocarFrequencias(float distancia);
+void tocarBeep(float distancia);
 
 void setup() {
   Serial.begin(9600);
@@ -28,19 +39,24 @@ void setup() {
   //configurando pino do Buzzer como saída
   pinMode(BUZZ_PIN,OUTPUT);
 
+  // interrupção do sensor de distancia - é acionada quando o sinal do pino Echo muda (CHANGE)
+  //attachInterrupt(0, intSensor, CHANGE);
+
+  //configurando pinos do led
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
 
-  pinMode(BUT_LEFT, INPUT);
-  pinMode(BUT_RIGHT, INPUT);
+  //configurando pinos dos botoes
+  pinMode(BUT_UP, INPUT);
+  pinMode(BUT_DOWN, INPUT);
+
+  //interrupção do botao - é acionada quando o botão passa pro estado HIGH (RISING)
+ attachInterrupt(1, intBotao, RISING );
 
 }
 
 void loop() {
-  int beep = digitalRead(BUT_LEFT);
-  int freq = digitalRead(BUT_RIGHT);
-  
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
@@ -53,65 +69,84 @@ void loop() {
  Serial.print(distancia);
  Serial.println(" cm");
 
- if(beep) {
+ if(modeBeep) {
    tocarBeep(distancia);
  } else {
    tocarFrequencias(distancia);
  }
- void tocarBeep(float distancia);
- void tocarFrequencias(float distancia);
 
+}
+
+void intBotao() {
+  //ativa modo frequencias
+  if(digitalRead(BUT_UP)) {
+    modeBeep = 0;   
+  }
+
+  //ativa modo beep
+  if(digitalRead(BUT_DOWN)) {
+    modeBeep = 1;
+  }
+  
 }
  
 void tocarBeep(float distancia) {
+   desligarLeds();
    if(distancia>20 && distancia<30) {
-    ascendeLed(0, 255, 0);  // green
+    corLed(0, 255, 0);  // green
     atraso = 2000;
    }
    else if(distancia >15 && distancia <20) {
-    ascendeLed(255, 255, 0);  // yellow
+    corLed(255, 255, 0);  // yellow
     atraso = 1400;
    }
    else if(distancia >10 && distancia<15){
-    ascendeLed(255, 255, 0);  // yellow
+    corLed(255, 255, 0);  // yellow
     atraso = 600;
    }
    else if(distancia < 10){
-    ascendeLed(255, 0, 0);  // red
-    atraso = 200;
+    corLed(255, 0, 0);  // red
+    atraso = 250;
    }
   
    if(distancia <40) {
-    tone(BUZZ_PIN, 2000, duration);
+    tone(BUZZ_PIN, 2000, 200);
    } else {
+    desligarLeds();
     noTone(BUZZ_PIN);
    }
+
+   delay(atraso);
 }
 
 
 void tocarFrequencias(float distancia) {
+   int value_delay = 1;
    if( distancia <= 30 && distancia > 2 ) {
-     int frequencia = map(distancia, 2, 30, 900, 50);
+     int frequencia = map(distancia, 2, 30, 900, 30);
+     if(distancia < 5) {
+       frequencia = 900;
+       value_delay = 150;
+     }
      tone(BUZZ_PIN, frequencia, 100);
      acendeLed(distancia);
    } else {
      desligarLeds();
      noTone(BUZZ_PIN);
    }
-    delay(1);
+    delay(value_delay);
   }
-}
 
 void acendeLed(float distancia) {
   desligarLeds();
   if(distancia < 30 && distancia > 20) {
-    ascendeLed(0, 255, 0);  // green
+    corLed(0, 255, 0);  // green
   }
   else if(distancia < 20 && distancia > 10) {
-    ascendeLed(255, 255, 0);  // yellow
+    corLed(255, 255, 0);  // yellow
   }
   else if (distancia < 10) {
-    ascendeLed(255, 0, 0);  // red
+    corLed(255, 0, 0);  // red
   }
 }
 
@@ -121,7 +156,7 @@ void desligarLeds() {
   digitalWrite(LED_BLUE, LOW);
 }
 
-void ascendeLed(int red, int green, int blue)
+void corLed(int red, int green, int blue)
 {
   analogWrite(LED_RED, red);
   analogWrite(LED_GREEN, green);
