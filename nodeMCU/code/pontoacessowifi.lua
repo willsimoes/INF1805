c2a0461eb63d8c73a940c782a2406298c045b987
Willian Simões <willian.smos@gmail.com>
2017-06-06 21:08:44 -0300

    reorganizando repo

---
+++ nodeMCU/code/pontoacessowifi.lua
@@ -0,0 +1,5 @@
+-- Conexao na rede Wifi
+wifi.setmode(wifi.SOFTAP)
+wifi.ap.config({ssid="noemi_nodemcu",pwd="blablabla"})
+wifi.ap.setip({ip="192.168.0.20",netmask="255.255.255.0",gateway="192.168.0.20"})
+print(wifi.ap.getip())
